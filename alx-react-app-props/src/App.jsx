d2f5953b75545d38bcb86155e12079bb5698a3e9
Eric Danquah <danquah@aircellsolutions.com>
2026-01-15 22:27:42 +0000

    Refactor prop and drilling to context API

---
+++ alx-react-app-props/src/App.jsx
@@ -0,0 +1,29 @@
+import UserProfile from './components/UserProfile';
+
+import Header from './components/Header';
+import MainContent from './components/MainContent';
+import Footer from './components/Footer';
+
+import './App.css'
+import WelcomeMessage from './components/WelcomeMessage';
+
+
+
+
+function App() {
+  return (
+    <div>
+      <UserProfile
+        name="Alice"
+        age="25"
+        bio="Loves hiking and photography"
+      />
+      <Header />
+      <MainContent />
+      <Footer />
+      <WelcomeMessage />
+    </div>
+  );
+}
+
+export default App;
