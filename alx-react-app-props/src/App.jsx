8ead05182c4c53bc0f02c59b5c1dfe4e4dd6019b
Eric Danquah <danquah@aircellsolutions.com>
2026-01-18 12:08:37 +0000

    Refactor prop drilling to context API

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -1,28 +1,40 @@
 import UserProfile from './components/UserProfile';
-
 import Header from './components/Header';
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
-
-import './App.css'
 import WelcomeMessage from './components/WelcomeMessage';
 
+import ProfilePage from './ProfilePage';
+import UserContext from './UserContext';
 
-
+import './App.css';
 
 function App() {
+  const userData = {
+    name: 'Jane Doe',
+    email: 'jane.doe@example.com',
+  };
+
   return (
-    <div>
-      <UserProfile
-        name="Alice"
-        age="25"
-        bio="Loves hiking and photography"
-      />
-      <Header />
-      <MainContent />
-      <Footer />
-      <WelcomeMessage />
-    </div>
+    <UserContext.Provider value={userData}>
+      <div>
+        <Header />
+        <MainContent />
+
+        <UserProfile
+          name="Alice"
+          age="25"
+          bio="Loves hiking and photography"
+        />
+
+        <WelcomeMessage />
+
+        {/* Context API usage */}
+        <ProfilePage />
+
+        <Footer />
+      </div>
+    </UserContext.Provider>
   );
 }
 
