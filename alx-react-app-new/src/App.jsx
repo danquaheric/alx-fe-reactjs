1c130ea8692cfb17f41d095b9a0507098b55624f
Eric Danquah <danquah@aircellsolutions.com>
2026-01-18 13:47:26 +0000

    Add UserContext.js

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -1,29 +1,13 @@
-import UserProfile from './components/UserProfile';
-import Header from './components/Header';
-import MainContent from './components/MainContent';
-import Footer from './components/Footer';
-import './App.css'
-import WelcomeMessage from './components/WelcomeMessage';
-import Counter from './components/Counter';
-
-
-
-
+import ProfilePage from './ProfilePage';
+import UserContext from './UserContext';
 
 function App() {
+  const userData = { name: 'Jane Doe', email: 'jane.doe@example.com' };
+
   return (
-    <div>
-      <UserProfile
-        name="Alice"
-        age="25"
-        bio="Loves hiking and photography"
-      />
-      <Header />
-      <MainContent />
-      <Footer />
-      <WelcomeMessage />
-      <Counter />
-    </div>
+    <UserContext.Provider value={userData}>
+      <ProfilePage />
+    </UserContext.Provider>
   );
 }
 
