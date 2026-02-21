b05648e6c3b1f3c73648bb4680650e12d7b682ef
Eric Danquah <danquah@aircellsolutions.com>
2026-02-21 07:51:36 +0000

    Advanced Data Handling with React Query

---
+++ react-query-demo/src/App.jsx
@@ -0,0 +1,28 @@
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import PostsComponent from './PostsComponent';
+
+const queryClient = new QueryClient();
+
+function Home() {
+  return <h2>Home Page</h2>;
+}
+
+function App() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Router>
+        <nav>
+          <Link to="/">Home</Link> | <Link to="/posts">Posts</Link>
+        </nav>
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/posts" element={<PostsComponent />} />
+        </Routes>
+      </Router>
+    </QueryClientProvider>
+  );
+}
+
+export default App;
